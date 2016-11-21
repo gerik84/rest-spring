919a67407c5b29aa5bf76edf92db06c1b80b1839
Gerasimchik Pavel <pavel.asdevel@gmail.com>
2016-11-21 09:59:56 +0400

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,33 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController  implements ErrorController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
